975456965a26117e2d49678084eeedd9aecd01c3
George Richardson <g.raymond.richardson@gmail.com>
2017-10-21 17:12:06 -0700

    full beneficiary url

--- source/crowdfunding/spiders/crowdrise.py
+++ source/crowdfunding/spiders/crowdrise.py
@@ -41,7 +41,7 @@ def parse_relative_date(possible_dates):
 class CrowdriseFundraiserSpider(scrapy.Spider):
     name = 'crowdrise'
     allowed_domains = ['crowdrise.com']
-    start_urls = ['https://crowdrise.com/']
+    site_url = 'https://crowdrise.com/'
     
     def __init__(self, categories=None, page_limit=None):
         self.page_limit = page_limit
@@ -132,7 +132,8 @@ class CrowdriseFundraiserSpider(scrapy.Spider):
             beneficiary_url = '//div[@class="row clickable-user-row" and descendant::span[contains(., "Benefiting Charity")]]//a/@href'
             beneficiary_url = response.xpath(beneficiary_url).extract_first()
 
-            
+        beneficiary_url = site_url.join(beneficiary_url)
+
         fundraiser = CrowdriseFundraiser(
                 url = url,
                 page_type = page_type,
